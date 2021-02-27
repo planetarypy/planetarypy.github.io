8d7fbaae505fe92cea3e91293917c765a492c63f
Andrew Annex <ama6fy@virginia.edu>
2021-02-27 13:25:33 -0500

    Update conf.py
    I think the site url needs to reflect the domain name, lots of github.io links are showing up

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "PlanetaryPy Technical Committee"  # (translatable)
 BLOG_TITLE = "PlanetaryPy"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://planetarypy.github.io/"
+SITE_URL = "https://planetarypy.org/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://planetarypy.github.io/"
